9c5439db374f77b42f89fee5fb3af1cb4cbddc97
Gabriel Brisolara <brisolarag@gmail.com>
2023-09-01 10:59:41 -0300

    mend
    Primeiro commit funcional

--- script/index.js
+++ script/index.js
@@ -1,20 +1,26 @@
-// rastreio teste = LB576309921HK
+// API
+// https://api.linketrack.com/track/json?user=dev.brisolara@gmail.com&token=813bebd95feb435f8ece25566bf763cfc82a15e5d53b581c299140ff21549d71&codigo=LB576309921HK
 
+// Rastreio Teste
+// LB576309921HK
 
 let trackBtn = document.querySelector('.track-btn');
-
-const user = '' // preciso receber no email
-const token = '' // preciso receber no email
-let codigoRastreio = document.querySelector('.track').value;
+let codigoRastreio = document.querySelector('.track');
 
 
 trackBtn.addEventListener('click', function() {
-    var url = 'https://api.linketrack.com/track/json?user=' + user + '&token=' + token + 'codigo=' + codigoRastreio;
+    var url = 'https://api.linketrack.com/track/json?user=dev.brisolara@gmail.com&token=813bebd95feb435f8ece25566bf763cfc82a15e5d53b581c299140ff21549d71&codigo=' + codigoRastreio.value
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data); // Mostra a resposta no console do navegador
+            let len = data.eventos.length
+            for (let i = 0 ; i < len ; i++) {
+                document.querySelector('.resultado-trackD' + i).textContent = data.eventos[i].local
+                document.querySelector('.resultado-trackS' + i).textContent = data.eventos[i].status
+                document.querySelector('.r' + i).style.display = 'table-row'
+            }
+
         })
         .catch(error => {
             console.error('Erro ao fazer a solicitação:', error);
